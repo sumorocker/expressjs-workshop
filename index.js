355f14e73661f4e49490d761b8b5c4eb8e188a61
sumorocker <macdonald.allan@live.com>
2015-11-30 18:58:13 +0000

    exercise-1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/hello', function (req, res) {
+  res.send('<h1>Hello World!</h1>');
 });
 
 
