ea8485ef3cc0f01aad5ac2dbab8a6322a75b54d8
sumorocker <macdonald.allan@live.com>
2015-12-01 00:21:23 +0000

    exercise-5

--- index.js
+++ index.js
@@ -1,38 +1,76 @@
 var express = require('express');
 var app = express();
 
-
-app.get('/entry/:entryId', function(req, res) {
-    var entryId = req.params.entryId;
-
-    var entries = {
-      1: {
-        firstName: 'Luke',
-        lastName: 'Skywalker',
-        emails: [{
-          type: 'home',
-          address: 'john@smith.com'
-        }, {
-          type: 'home',
-          address: 'jsmith@megacorp.com'
-        }]
-      },
+function emailMatches(queryEmail,userEmails){
+  for(var i=0; i > userEmails.length; i++){
+    if (userEmails[i].address === queryEmail){
+      return true;
     }
 
-    if (entries[entryId]) {
-      res.json(entries[entryId])
+  }
+  return false;
+}
+
+app.get('/entry/', function(req, res) {
+      var matches = []
+      var entries = {
+        1: {
+          firstName: 'Luke',
+          lastName: 'Skywalker',
+          emails: [{
+            type: 'home',
+            address: 'john@smith.com'
+          }, {
+            type: 'home',
+            address: 'jsmith@megacorp.com'
+          }]
+        },
+        2: {
+          firstName: 'Luke',
+          lastName: 'Earthflier',
+          emails: [{
+            type: 'home',
+            address: 'theforce@smith.com'
+          }, {
+            type: 'home',
+            address: 'darthsucks@megacorp.com'
+          }]
+        },
+        3: {
+          firstName: 'Leia',
+          lastName: 'Organa',
+          emails: [{
+            type: 'home',
+            address: 'princess@live.com'
+          }, {
+            type: 'work',
+            address: 'jabbasucks@megacorp.com'
+          }]
+        },
+      }
+
+ 
+      for (var prop in entries) {
+
+        if (req.query.firstName === entries[prop].firstName || req.query.lastName === entries[prop].lastName || emailMatches(req.query.email, entries[prop].emails)) {
+          matches.push(entries[prop])
+        }
     }
-    else {
-      res.send('Error 404')
-    }
-  })
+    res.json(matches)
 
-//YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) 
 
-// Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
+
+app.listen(process.env.PORT, function() {
+  console.log("server started on port " + process.env.PORT)
+});
+
+    //YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) 
+
+    // Boilerplate code to start up the web server
+    // var server = app.listen(process.env.PORT, process.env.IP, function() {
+    //   var host = server.address().address;
+    //   var port = server.address().port;
+
+    //   console.log('Example app listening at http://%s:%s', host, port);
+    // });
